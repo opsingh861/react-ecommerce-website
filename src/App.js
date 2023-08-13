bb3ef8531a091c3990ad81e77e8e440ffce0ed21
Aditya Dhanraj <opsingh861@gmail.com>
2023-08-13 12:41:56 +0530

    pages and some components are added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
+import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
 function App() {
   return (
     <>
-
+      <Home />
     </>
   );
 }
